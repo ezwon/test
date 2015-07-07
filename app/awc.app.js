dbf9a1b2265ca285d6dde56d13770274e3aa9a36
Jayson Daquer <me@jaysondaquer.com>
2015-07-07 21:52:14 +0800

    Ongoing Magic on Schedule

--- app/awc.app.js
+++ app/awc.app.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
     angular
-        .module('AffiliateWorldAsia', ['ngRoute','ngSanitize', 'ngAnimate', 'pascalprecht.translate', 'timer', 'ngDialog','duScroll'])
+        .module('AffiliateWorldAsia', ['ngRoute', 'ngSanitize', 'ngLodash', 'ngAnimate', 'pascalprecht.translate', 'timer', 'ngDialog', 'duScroll'])
         .controller('ScheduleCtrl', ScheduleCtrl)
         .controller('LocationCtrl', LocationCtrl)
         .filter('timeLineStart', timeLineStart)
@@ -12,7 +12,7 @@
         .directive('langMenu', langMenu)
         .service('apiService', apiService);
 
-    function ScheduleCtrl($scope, $timeout, $window, apiService, $routeParams, $location, $document, $anchorScroll) {
+    function ScheduleCtrl($scope, $timeout, $window, apiService, $routeParams, $location, $document, lodash) {
         var w = angular.element($window);
         w.bind('resize', function () {
             $scope.$apply();
@@ -74,19 +74,21 @@
         function UpdateSizeWindow() {
             $scope.UpdateSize($scope.selectedIndex);
         }
+
         apiService.get().then(function (response) {
             var event = response.data.data,
                 tabs = [],
                 itineraryList = [],
                 schedule = null,
-                timeEntries = [];
-
+                timeEntries = [],
+                uniqueItineraries = [];
 
             console.log("Service Response: ", event)
             for (var i = 0; i < event.schedules.length; i++) {
                 schedule = event.schedules[i];
                 schedule.timeLineEntries = [];
                 itineraryList = [];
+                uniqueItineraries = [];
 
                 for (var j = 0; j < schedule.itineraries.length; j++) {
                     var itinerary = schedule.itineraries[j];
@@ -95,22 +97,23 @@
                     for (var k = 0; k < itinerary.entries.length; k++) {
                         var entry = itinerary.entries[k];
                         var timeEntry = {
-                            id:k+1,
-                            itineraryId:j+1,
+                            id: k + 1,
+                            itineraryId: j + 1,
                             itinerary: itinerary.title,
-                            colorScheme:itinerary.color_scheme,
-                            isKeynote:entry.is_keynote == '1' ? true:false,
+                            colorScheme: itinerary.color_scheme,
+                            isKeynote: entry.is_keynote == '1' ? true : false,
                             title: entry.title,
                             showDetails: false,
                             secondTitle: entry.second_title,
                             websiteUrl: entry.website,
-                            facebookUrl:entry.fb_link,
-                            twitterUrl:entry.twitter_link,
-                            instagramUrl:entry.instagram_link,
+                            facebookUrl: entry.fb_link,
+                            twitterUrl: entry.twitter_link,
+                            instagramUrl: entry.instagram_link,
                             imageUrl: entry.image_url,
                             shortDescription: entry.short_description,
                             longDescription: entry.long_description,
                             timeStr: moment(entry.time_starts, "hh:mm").format('hh:mma') + " - " + moment(entry.time_ends, "hh:mm").format('hh:mma'),
+                            orderId: entry.id,
                             timeStart: moment(eventDate.format("MM/DD/YYYY") + " " + entry.time_starts, "MM/DD/YYYY hh:mm"),
                             timeEnds: moment(eventDate.format("MM/DD/YYYY") + " " + entry.time_ends, "MM/DD/YYYY hh:mm"),
                             duration: moment(entry.time_ends, "hh:mm").diff(moment(entry.time_starts, "hh:mm"), 'minutes')
@@ -120,19 +123,33 @@
                         schedule.timeLineEntries.push(timeEntry);
                     }
 
+
+                    if (lodash.findIndex(uniqueItineraries, function (i) {
+                            return i.title == itinerary.title
+                        }) == -1) {
+                        uniqueItineraries.push({
+                            title: itinerary.title,
+                            timeEntries: []
+                        })
+                    }
+
+
                     itineraryList.push({
-                        id: j+1,
+                        id: j + 1,
                         title: itinerary.title,
                         enabled: itinerary.enabled == "1" ? true : false,
                         cssClass: '',
                         colorScheme: itinerary.color_scheme,
                         timeEntries: timeEntries
                     });
+
+
                 }
 
-                schedule.timeLineEntries.sort( function (a,b) {
-                    return a.timeStart > b.timeStart;
-                })
+                //schedule.timeLineEntries.sort( function (a,b) {
+                //    return parseInt(a.orderId) > parseInt(b.orderId);
+                //})
+
 
                 tabs.push({
                     scheduleID: i + 1,
@@ -140,12 +157,32 @@
                     isSelected: i == 0 ? true : false,
                     scheduleDateStr: moment(schedule.date_time_start, "MM/DD/YYYY hh:mm").format('dddd, D MMMM'),
                     timeLineStart: moment(schedule.date_time_start, "MM/DD/YYYY hh:mm"),
-                    itinerary: itineraryList
+                    itinerary: itineraryList,
+                    uniqueItinerary: uniqueItineraries
                 });
 
 
-
             }
+
+            for (var i = 0; i < tabs.length; i++) {
+                var schedule = tabs[i];
+
+                for (var j = 0; j < schedule.timeLineEntries.length; j++) {
+                    var entry = schedule.timeLineEntries[j];
+
+                    console.log(entry.itinerary);
+
+                    for (var k = 0; k < schedule.uniqueItinerary.length; k++) {
+                        var uniqueItinerary = schedule.uniqueItinerary[k];
+
+                        if(entry.itinerary == uniqueItinerary.title)
+                            uniqueItinerary.timeEntries.push(entry);
+                        
+                    }
+
+                }
+            }
+
 
 
             if ($routeParams.schedule)
@@ -155,9 +192,7 @@
                         tabs[i].isSelected = true;
                 }
 
-
-
-            console.log("Parsed Schedules: ",tabs);
+            console.log("Parsed Schedules: ", tabs);
             $scope.schedules = tabs;
             $scope.selectedIndex = 1;
             $timeout(function () {
@@ -295,11 +330,11 @@
 
     function timeCenterPopUp() {
         return function (input, width) {
-            if(width < 410){
+            if (width < 410) {
                 width = (410 - width) / 2;
                 return width * -1;
             }
-            else{
+            else {
                 width = (width - 410) / 2;
                 return width
             }
@@ -315,48 +350,73 @@
                 AvatarUrl: "assets/images/speakers/portrait-eric-whitman.png",
                 Description: "Eric Whitman Description",
                 OtherInfo: "Second paragraph to go here.",
-                SocialLinks:{
-                    facebook:'',
-                    twitter:'',
-                    website:''
+                SocialLinks: {
+                    facebook: '',
+                    twitter: '',
+                    website: ''
                 }
             }
         ];
         var mockSponsors = [
-           {
+            {
                 Name: "Tim Tetra",
                 Position: "CMO/Co-founder of Tetra Communications",
                 AvatarUrl: "assets/images/speakers/portrait-tim-tetra.png",
                 Description: "Tim Tetra is a serial entrepreneur whose ads reached over 337 million people in 173 countries worldwide in 2014.  Tim has integrated lessons learned from founding six successful businesses to create inimitable edges that separate his company from the rest of the affiliate marketing pack.",
-                OtherInfo: $sce.trustAsHtml("In his spare time, Tim is a huge advocate of the all-or-nothing mindset.  It is not uncommon for him to pick up a new hobby, spend 80 hours a week immersed in it with the best mentors out there, and then mastering it in record time.")
+                OtherInfo: $sce.trustAsHtml("In his spare time, Tim is a huge advocate of the all-or-nothing mindset.  It is not uncommon for him to pick up a new hobby, spend 80 hours a week immersed in it with the best mentors out there, and then mastering it in record time."),
+                SocialLinks: {
+                    facebook: '',
+                    twitter: '',
+                    website: ''
+                }
             },
             {
                 Name: "Charles Ngo",
                 Position: "Writer and Internet Marketer",
                 AvatarUrl: "assets/images/speakers/portrait-charles-ngo.png",
                 Description: "Charles Ngo is one of the most well-known and respected internet marketers in the world generating several millions dollars in profit within his first year of starting.",
-                OtherInfo: $sce.trustAsHtml("He has gone on to become one of the leading authorities in affiliate marketing information. He and his team specialise in traffic sources such as Facebook, mobile, and display buys.")
+                OtherInfo: $sce.trustAsHtml("He has gone on to become one of the leading authorities in affiliate marketing information. He and his team specialise in traffic sources such as Facebook, mobile, and display buys."),
+                SocialLinks: {
+                    facebook: '',
+                    twitter: '',
+                    website: ''
+                }
             },
             {
                 Name: "Lorenzo Green",
                 Position: "Co-director Stack That Money Forums",
                 AvatarUrl: "assets/images/speakers/portrait-lorenzo-green.png",
                 Description: "Lorenzo Green co-directs Stack That Money forums, the number one rated affiliate forum and is a veteran in the industry with more than nine years experience. He is known for his marketing creativity, and constantly being at the forefront of the next big thing in the affiliate space. This combination has yielded him consistent seven figure/year profits with his affiliate operations.",
-                OtherInfo: $sce.trustAsHtml("He has also founded a number of other successful business including a successful affiliate network and <a href='http://www.greenmantle.co.nz/' target='_blank'>Greenmantle Estate</a>, one of the most prestigious luxury lodges in his home country, New Zealand.")
+                OtherInfo: $sce.trustAsHtml("He has also founded a number of other successful business including a successful affiliate network and <a href='http://www.greenmantle.co.nz/' target='_blank'>Greenmantle Estate</a>, one of the most prestigious luxury lodges in his home country, New Zealand."),
+                SocialLinks: {
+                    facebook: '',
+                    twitter: '',
+                    website: ''
+                }
             },
             {
                 Name: "Hugh Hancock",
                 Position: "Founder of Strange Company",
                 AvatarUrl: "assets/images/speakers/portrait-hugh-hancock.png",
                 Description: "TBC.",
-                OtherInfo: ""
+                OtherInfo: "",
+                SocialLinks: {
+                    facebook: '',
+                    twitter: '',
+                    website: ''
+                }
             },
             {
                 Name: "Besmir B",
                 Position: "Co-director Stack That Money Forums",
                 AvatarUrl: "assets/images/speakers/portrait-besimir-b.png",
                 Description: "Coming from a technical background, Besmir found affiliate marketing by pure coincidence. Besmir has been in the industry long enough to see many trends come and go. He understands that it's not about that special niche or traffic source but it's all about persistence, hard work and loving what you do.",
-                OtherInfo: $sce.trustAsHtml("Lately he has been focusing on mobile marketing and investing on building new and successful companies including Stack That Money, the number one rated forum in the affiliate industry, which he is co-director of.")
+                OtherInfo: $sce.trustAsHtml("Lately he has been focusing on mobile marketing and investing on building new and successful companies including Stack That Money, the number one rated forum in the affiliate industry, which he is co-director of."),
+                SocialLinks: {
+                    facebook: '',
+                    twitter: '',
+                    website: ''
+                }
             }
         ]
         var svc = {
