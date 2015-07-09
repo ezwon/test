d27db987e2255009e842ff899f5826820b425cea
Von Carcha <vhoncarcha420@gmail.com>
2015-07-09 13:27:16 +0800

    implementing GSAP

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+// GSAP scripts
+TweenLite.set("#page-1", {x:0, opacity:1, zIndex:3});
+TweenLite.set("#page-2", {x:0, opacity:0, zIndex:2});
+TweenLite.set("#page-3", {x:0, opacity:0, zIndex:1});
+
+$('.skip-step').on('click',
+  function() {
+    TweenMax.to("#page-2", 0.5, {opacity:1, zIndex:2, ease:Power4.easeInOut});
+    TweenMax.to("#page-1", 0.5, {opacity:0, zindex:1, ease:Power4.easeInOut});
+  });
 
 
 // scripts for schedule page
@@ -15,3 +25,4 @@ var share = $(".share-trigger");
 		if (skip.hasClass("skip-step"))
 			skip.css({display: "none"})
 	});
+
